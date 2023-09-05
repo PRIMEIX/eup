cb8454118ac29c44c7631d5ad25c58d90c96439a
Erasure <ahjinguildgaming@gmail.com>
2023-09-05 07:33:06 +0530

    update

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+white 'White Custom'
+discord 'https://discord.gg/cUxnazUxqU'
+
+files {
+  'mp_m_freemode_01_mp_m_busmods.meta',
+  'mp_f_freemode_01_mp_f_busmods.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_busmods.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_busmods.meta'
